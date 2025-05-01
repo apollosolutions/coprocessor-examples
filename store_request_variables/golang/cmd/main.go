8aeeebabbfbebad1915e89feb1dfe3dbf965b784
Daniel Dorsey <daniel.dorsey@apollographql.com>
2025-05-01 12:20:13 -0500

    Remove unused functions

--- store_request_variables/golang/cmd/main.go
+++ store_request_variables/golang/cmd/main.go
@@ -1,26 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"github.com/rs/zerolog/log"
 	"net/http"
 
 	coprocessor "example.com/variables/internal"
 )
-
-func hello(w http.ResponseWriter, req *http.Request) {
-
-	fmt.Fprintf(w, "hello\n")
-}
-
-func headers(w http.ResponseWriter, req *http.Request) {
-
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
-	}
-}
 
 func main() {
 
